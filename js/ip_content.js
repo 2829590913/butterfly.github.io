caafbe4cd1733907584c20a7993d06803c5a17dc
2829590913 <2829590913@qq.COM>
2022-02-19 01:15:11 +0800

    Site updated: 2022-02-19 01:15:07

--- js/ip_content.js
+++ js/ip_content.js
@@ -33,9 +33,3 @@ var ip_content = document.querySelector(".ip_content");
 if (ip_content != null && typeof (returnCitySN) != undefined) {
     ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN["cname"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["cip"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
 }
-
-
-作者: 花猪
-链接: https://cnhuazhu.gitee.io/2021/05/28/Hexo%E9%AD%94%E6%94%B9/Hexo%E6%B7%BB%E5%8A%A0%E5%BD%93%E5%89%8D%E8%AE%BF%E5%AE%A2%E4%BF%A1%E6%81%AF/
-来源: 花猪のBlog
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
